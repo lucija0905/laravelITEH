3d8dcb57411345b639116c0179464b1524e9d900
lucija0905 <lm20190223@student.fon.bg.ac.rs>
2023-02-10 18:45:18 +0100

    Dodata pocetna stranica

--- frontapp/src/Komponente/Footer.jsx
+++ frontapp/src/Komponente/Footer.jsx
@@ -39,9 +39,7 @@ function Footer() {
       <div className="container">
         <div className="row">
           <div className="col-md-8 col-sm-6 col-xs-12">
-            <p className="copyright-text">Copyright &copy; 2022 All Rights Reserved by   
-         <a href="#"> Nadja & Marko & Kristina</a>.
-            </p>
+            <p className="copyright-text">Copyright &copy; 2023</p>
           </div>
 
          
