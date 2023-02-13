3cc06c30a2eb112276d74dd45d38d41a345ef0b6
lucija0905 <lm20190223@student.fon.bg.ac.rs>
2023-02-13 19:04:13 +0100

    login i registracija

--- frontapp/src/App.js
+++ frontapp/src/App.js
@@ -1,15 +1,29 @@
  
 import './App.css';
 import Footer from './Komponente/Footer';
+import Login from './Komponente/Login';
 import NavBar from './Komponente/NavBar';
 import Pocetna from './Komponente/Pocetna';
-
+import Register from './Komponente/Register';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 function App() {
+  const[token,setToken] = useState();
+   
+  function addToken(auth_token){
+    setToken(auth_token);
+}
   return (
     <div  >
+      <BrowserRouter className="App">
         <NavBar></NavBar>
-        <Pocetna></Pocetna>
+        <Routes>
+            <Route path="/" element={ <Pocetna></Pocetna>}></Route>
+            <Route path="/login" element={ <Login  addToken={addToken} ></Login>}></Route>
+            <Route path="/register" element={ <Register ></Register>}></Route>
+        </Routes>
         <Footer></Footer>
+        </BrowserRouter>
     </div>
   );
 }
